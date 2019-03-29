11b556c7540d7b05b5163196ed2a4c3fd38c3a2b
LeonineKing1199 <christian.mazakas@gmail.com>
2019-03-28 19:19:36 -0700

    Fixed comments

--- src/client_session.cpp
+++ src/client_session.cpp
@@ -1,8 +1,8 @@
-// // github.com/LeonineKing1199/foxy
+//
 // Copyright (c) 2018-2019 Christian Mazakas (christian dot mazakas at gmail dot com)
 //
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 // Official repository: https://github.com/LeonineKing1199/foxy
 //
@@ -10,6 +10,6 @@
 #include <foxy/client_session.hpp>
 
 foxy::client_session::client_session(boost::asio::io_context& io, session_opts opts)
-: session(io, std::move(opts))
+  : session(io, std::move(opts))
 {
 }
